a2c8c1d69ff77edf5e8db4305570e591855928d6
RMvirtual <rmvirtual@AOL.com>
2021-01-05 17:31:52 +0000

    Refactoring of settings window with multiple inherticance used for some of the widgets and panels etc.

--- G-Scan/src/main/python/settingswindowgui.py
+++ G-Scan/src/main/python/settingswindowgui.py
@@ -5,6 +5,12 @@ class SettingsWindowGUI(wx.Frame):
 
     def __init__(self, main_application):
         """Constructor method."""
+        self.__main_application = main_application
+        self.__current_user = self.__main_application.get_current_user()
+        self.__create_widgets()
+   
+    def __create_widgets(self):
+        """Creates the widgets required for the settings GUI."""
         super().__init__(
             None,
             size = (895, 270),
@@ -12,20 +18,61 @@ class SettingsWindowGUI(wx.Frame):
         )
         
         self.SetBackgroundColour("WHITE")
-        self.__main_application = main_application
-        self.__current_user = self.__main_application.get_current_user()
-        self.__create_widgets()
-   
-    def __create_widgets(self):
-        """Creates the widgets required for the settings GUI."""
-
-        self.__set_fonts()        
-        self.__create_text_values_panel()
-        self.__create_mode_options_panel()
-        self.__create_buttons_panel()
+        self.__text_values_panel = TextValuesPanel(self)
+        self.__mode_options_panel = ModeOptionsPanel(self)
+        self.__buttons_panel = ButtonsPanel(self)
         self.Show()
 
-    def __set_fonts(self):
+class TextLabel(wx.StaticText):
+    """A class representing a text label (showing a heading of data
+    rather than data itself)."""
+
+    def __init__(self, text, panel, label_position, label_size, font):
+        """Constructor method."""
+
+        super().__init__(
+            panel,
+            label = text,
+            pos = label_position,
+            size = label_size,
+            style = wx.BORDER_NONE
+        )
+
+        self.SetFont(font)
+
+class TextField(wx.StaticText):
+    """A class representing a text field (showing data rather than a
+    heading)."""
+
+    def __init__(self, text, panel, field_position, field_size, font):
+        """Constructor method."""
+
+        super().__init__(
+            panel,
+            label = text,
+            pos = field_position,
+            size = field_size,
+            style = wx.BORDER_SIMPLE
+        )
+
+        self.SetFont(font)
+        self.SetBackgroundColour("LIGHT GREY")
+
+class SettingsWindowPanel(wx.Panel):
+    """A class representing a panel in the settings window."""
+
+    def __init__(self, frame, panel_size, panel_position):
+        """Constructor method."""
+
+        super().__init__(
+            frame,
+            size = panel_size,
+            pos = panel_position
+        )
+
+        self.__initialise_fonts()
+
+    def __initialise_fonts(self):
         """Sets the fonts to be used for the widget types."""
 
         self.__button_font = wx.Font(
@@ -34,115 +81,88 @@ class SettingsWindowGUI(wx.Frame):
         self.__body_font = wx.Font(
             14, wx.FONTFAMILY_MODERN, wx.NORMAL, wx.NORMAL, False, u"calibri")
 
-    def __create_text_values_panel(self):
-        """Creates the text values panel that all the text-based value related
-        widgets (i.e user name and directory locations) sit within inside
-        the window."""
+    def get_body_font(self):
+        """Returns the font for body text."""
 
-        # Text Values panel for holding the widgets.
-        self.__text_values_panel = wx.Panel(
-            self,
-            size = (860, 125),
-            pos = (10, 10)
+        return self.__body_font
+
+    def get_buttons_font(self):
+        """Returns the font for button text."""
+
+        return self.__button_font
+
+class TextValuesPanel(SettingsWindowPanel):
+    """A class representing the text values panel in the settings
+    window GUI."""
+
+    def __init__(self, frame):
+        """Constructor method."""
+
+        super().__init__(
+            frame,
+            panel_size = (860, 125),
+            panel_position = (10, 10)
         )
 
+        self.__frame = frame
         self.__create_user_name_widgets()
         self.__create_scan_directory_widgets()
         self.__create_destination_directory_widgets()
         self.__create_backup_directory_widgets()
 
-    def __create_mode_options_panel(self):
-        """Creates the mode options panel that all the drop-down menu
-        value related widgets (i.e. paperwork type,
-        multi-page handling, input mode and POD autoprocessing) sit
-        withing inside the window."""
-
-        # Mode Options panel for holding the widgets.
-        self.__mode_options_panel = wx.Panel(
-            self,
-            size = (860, 55),
-            pos = (10, 135)
-        )
-
-        self.__create_paperwork_type_widgets()
-        self.__create_multi_page_handling_widgets()
-        self.__create_input_mode_widgets()
-        self.__create_autoprocessing_widgets()
-
-    def __create_buttons_panel(self):
-        """Creates a panel for setting the buttons."""
-
-        self.__buttons_panel = wx.Panel(
-            self,
-            size = (860, 30),
-            pos = (10, 195)
-        )
-
-        self.__create_buttons()
-
     def __create_user_name_widgets(self):
         """Creates widgets related to the name of the current user."""
 
         # User Name label.
-        self.__user_name_label = wx.StaticText(
-            self.__text_values_panel,
-            label = "User Name:",
-            pos = (0, 0),
-            size = (200, 20),
-            style = wx.BORDER_NONE
+        self.__user_name_label = TextLabel(
+            text = "User Name:",
+            panel = self,
+            label_position = (0, 0),
+            label_size = (200, 20),
+            font = self.get_body_font()
         )
-
-        self.__user_name_label.SetFont(self.__body_font)
 
         # User Name value label.
-        self.__user_name_text_ctrl = wx.StaticText(
-            self.__text_values_panel,
-            label = self.__current_user.get_name(),
-            pos = (200, 0),
-            size = (285, 25),
-            style = wx.BORDER_SIMPLE
+        self.__user_name_text_ctrl = TextField(
+            text = "",
+            panel = self,
+            field_position = (200, 0),
+            field_size = (285, 25),
+            font = self.get_body_font()
         )
-
-        self.__user_name_text_ctrl.SetFont(self.__body_font)
-        self.__user_name_text_ctrl.SetBackgroundColour("LIGHT GREY")
 
     def __create_scan_directory_widgets(self):
         """Creates widgets related to the current specified
         scan directory."""
 
         # Scan Directory label.
-        self.__scan_directory_label = wx.StaticText(
-            self.__text_values_panel,
-            label = "Scan Directory:",
-            pos = (0, 30),
-            size = (200, 20),
-            style = wx.BORDER_NONE
+        self.__scan_directory_label = TextLabel(
+            text = "Scan Directory:",
+            panel = self,
+            label_position = (0, 30),
+            label_size = (200, 20),
+            font = self.get_body_font()
         )
-
-        self.__scan_directory_label.SetFont(self.__body_font)
 
         # Scan Directory value text box.
-        self.__scan_directory_value_text_box = wx.StaticText(
-            self.__text_values_panel,
-            label = self.__current_user.get_name(),
-            pos = (200, 30),
-            size = (625, 25),
-            style = wx.BORDER_SIMPLE
+        self.__scan_directory_value_text_box = TextField(
+            text = "",
+            panel = self,
+            field_position = (200, 30),
+            field_size = (625, 25),
+            font = self.get_body_font()
         )
-
-        self.__scan_directory_value_text_box.SetFont(self.__body_font)
-        self.__scan_directory_value_text_box.SetBackgroundColour("LIGHT GREY")
 
         # Scan Directory file dialog button.
         self.__scan_directory_file_dialog_button = wx.Button(
-            self.__text_values_panel,
+            self,
             label = "...",
             pos = (826, 30),
             size = (25, 25)
         )
 
         self.__scan_directory_file_dialog_button.SetFont(
-            self.__button_font)
+            self.get_buttons_font())
 
     def __create_destination_directory_widgets(self):
         """Creates widgets related to the current specified
@@ -150,38 +170,41 @@ class SettingsWindowGUI(wx.Frame):
 
         # Destination Directory label.
         self.__destination_directory_label = wx.StaticText(
-            self.__text_values_panel,
+            self,
             label = "Destination Directory:",
             pos = (0, 60),
             size = (200, 20),
             style = wx.BORDER_NONE
         )
 
-        self.__destination_directory_label.SetFont(self.__body_font)
+        self.__destination_directory_label.SetFont(
+            self.get_body_font())
 
         # Destination Directory value text box.
         self.__destination_directory_value_text_box = wx.StaticText(
-            self.__text_values_panel,
-            label = self.__current_user.get_name(),
+            self,
+            label = "",
             pos = (200, 60),
             size = (625, 25),
             style = wx.BORDER_SIMPLE
         )
 
-        self.__destination_directory_value_text_box.SetFont(self.__body_font)
+        self.__destination_directory_value_text_box.SetFont(
+            self.get_body_font())
+        
         self.__destination_directory_value_text_box.SetBackgroundColour(
             "LIGHT GREY")
 
         # Destination Directory file dialog button.
         self.__destination_directory_file_dialog_button = wx.Button(
-            self.__text_values_panel,
+            self,
             label = "...",
             pos = (826, 60),
             size = (25, 25)
         )
 
         self.__destination_directory_file_dialog_button.SetFont(
-            self.__button_font)
+            self.get_buttons_font())
 
     def __create_backup_directory_widgets(self):
         """Creates widgets related to the current specified backup
@@ -189,58 +212,74 @@ class SettingsWindowGUI(wx.Frame):
 
         # Backup Directory label.
         self.__backup_directory_label = wx.StaticText(
-            self.__text_values_panel,
+            self,
             label = "Backup Directory:",
             pos = (0, 90),
             size = (200, 20),
             style = wx.BORDER_NONE
         )
 
-        self.__backup_directory_label.SetFont(self.__body_font)
+        self.__backup_directory_label.SetFont(
+            self.get_body_font())
 
         # Backup Directory value text box.
         self.__backup_directory_value_text_box = wx.StaticText(
-            self.__text_values_panel,
-            label = self.__current_user.get_name(),
+            self,
+            label = "",
             pos = (200, 90),
             size = (625, 25),
             style = wx.BORDER_SIMPLE
         )
 
-        self.__backup_directory_value_text_box.SetFont(self.__body_font)
+        self.__backup_directory_value_text_box.SetFont(
+            self.get_body_font())
+        
         self.__backup_directory_value_text_box.SetBackgroundColour(
             "LIGHT GREY")
 
         # Backup Directory file dialog button.
         self.__backup_directory_file_dialog_button = wx.Button(
-            self.__text_values_panel,
+            self,
             label = "...",
             pos = (826, 90),
             size = (25, 25)
         )
 
         self.__backup_directory_file_dialog_button.SetFont(
-            self.__button_font)
+            self.get_buttons_font())
 
+class ModeOptionsPanel(SettingsWindowPanel):
+    """A class representing the mode options panel."""
+
+    def __init__(self, frame):
+        super().__init__(
+            frame,
+            panel_size = (860, 30),
+            panel_position = (10, 135)
+        )
+
+        self.__frame = frame
+        self.__create_paperwork_type_widgets()
+        self.__create_multi_page_handling_widgets()
+        self.__create_input_mode_widgets()
+        self.__create_autoprocessing_widgets()
 
     def __create_paperwork_type_widgets(self):
         """Creates widgets related to the default paperwork type
         value."""
 
         # Paperwork Type label.
-        self.__paperwork_type_label = wx.StaticText(
-            self.__mode_options_panel,
-            label = "Default Paperwork Type:",
-            pos = (0, 0),
-            size = (200, 20),
-            style = wx.BORDER_NONE
+        self.__paperwork_type_label = TextLabel(
+            text = "Default Paperwork Type:",
+            panel = self,
+            label_position = (0, 0),
+            label_size = (200, 20),
+            font = self.get_body_font()
         )
-
-        self.__paperwork_type_label.SetFont(self.__body_font)
 
         # Paperwork Type value dropdown box.
         self.__paperwork_type_dropdown_box = wx.ComboBox(
-            self.__mode_options_panel,
+            self,
             value = "Customer PW",
             size = (120, 25),
             pos = (200, 0),
@@ -248,7 +287,7 @@ class SettingsWindowGUI(wx.Frame):
             style = wx.CB_DROPDOWN | wx.CB_READONLY
         )
 
-        self.__paperwork_type_dropdown_box.SetFont(self.__button_font)
+        self.__paperwork_type_dropdown_box.SetFont(self.get_buttons_font())
         self.__paperwork_type_dropdown_box.SetBackgroundColour("LIGHT GREY")
 
     def __create_multi_page_handling_widgets(self):
@@ -257,18 +296,18 @@ class SettingsWindowGUI(wx.Frame):
 
         # Multi-Page Handling label.
         self.__multi_page_handling_label = wx.StaticText(
-            self.__mode_options_panel,
+            self,
             label = "Multi-Page Handling:",
             pos = (330, 0),
             size = (165, 20),
             style = wx.BORDER_NONE
         )
 
-        self.__multi_page_handling_label.SetFont(self.__body_font)
+        self.__multi_page_handling_label.SetFont(self.get_body_font())
 
         # Multi-Page Handling default value dropdown box.
         self.__multi_page_handling_dropdown_box = wx.ComboBox(
-            self.__mode_options_panel,
+            self,
             value = "Split",
             pos = (500, 0),
             size = (120, 25),
@@ -276,7 +315,7 @@ class SettingsWindowGUI(wx.Frame):
             style = wx.CB_DROPDOWN | wx.CB_READONLY
         )
 
-        self.__multi_page_handling_dropdown_box.SetFont(self.__button_font)
+        self.__multi_page_handling_dropdown_box.SetFont(self.get_buttons_font())
         self.__multi_page_handling_dropdown_box.SetBackgroundColour(
             "LIGHT GREY")
 
@@ -285,18 +324,18 @@ class SettingsWindowGUI(wx.Frame):
 
         # Input Mode label.
         self.__input_mode_label = wx.StaticText(
-            self.__mode_options_panel,
+            self,
             label = "Input Mode:",
             pos = (630, 0),
             size = (80, 20),
             style = wx.BORDER_NONE
         )
 
-        self.__input_mode_label.SetFont(self.__body_font)
+        self.__input_mode_label.SetFont(self.get_body_font())
 
         # Input Mode default value dropdown box.
         self.__input_mode_dropdown_box = wx.ComboBox(
-            self.__mode_options_panel,
+            self,
             value = "Normal",
             pos = (735, 0),
             size = (120, 25),
@@ -304,7 +343,7 @@ class SettingsWindowGUI(wx.Frame):
             style = wx.CB_DROPDOWN | wx.CB_READONLY
         )
 
-        self.__input_mode_dropdown_box.SetFont(self.__button_font)
+        self.__input_mode_dropdown_box.SetFont(self.get_buttons_font())
         self.__input_mode_dropdown_box.SetBackgroundColour(
             "LIGHT GREY")
 
@@ -314,7 +353,7 @@ class SettingsWindowGUI(wx.Frame):
 
         # Autoprocessing checkbox.
         self.__autoprocessing_checkbox = wx.CheckBox(
-            self.__mode_options_panel,
+            self,
             label = "POD Autoprocessing",
             size = (160, 25),
             pos = (200, 30)
@@ -323,17 +362,30 @@ class SettingsWindowGUI(wx.Frame):
         self.__autoprocessing_checkbox.SetFont(wx.Font(
             9, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u"calibri"))
 
+class ButtonsPanel(SettingsWindowPanel):
+    """A class representing the buttons panel."""
+
+    def __init__(self, frame):
+        super().__init__(
+            frame,
+            panel_size = (860, 30),
+            panel_position = (10, 195)
+        )
+
+        self.__frame = frame
+        self.__create_buttons()
+
     def __create_buttons(self):
         """Creates widgets for the Save and Cancel buttons."""
 
         # Save button.
         self.__save_button = wx.Button(
-            self.__buttons_panel,
+            self,
             label = "Save",
             size = (60, 25),
             pos = (200, 0))
 
-        self.__save_button.SetFont(self.__button_font)
+        self.__save_button.SetFont(self.get_buttons_font())
 
         self.__save_button.Bind(
             wx.EVT_BUTTON,
@@ -342,12 +394,12 @@ class SettingsWindowGUI(wx.Frame):
 
         # Cancel button.
         self.__cancel_button = wx.Button(
-            self.__buttons_panel,
+            self,
             label = "Cancel",
             size = (60, 25),
             pos = (270, 0))
 
-        self.__cancel_button.SetFont(self.__button_font)
+        self.__cancel_button.SetFont(self.get_buttons_font())
 
         self.__cancel_button.Bind(
             wx.EVT_BUTTON,
@@ -358,10 +410,10 @@ class SettingsWindowGUI(wx.Frame):
         """Performs the behaviour required when the save button is
         clicked."""
 
-        wx.CallAfter(self.Destroy)
+        wx.CallAfter(self.__frame.Destroy)
 
     def __cancel_button_click(self, event = None):
         """Performs the behaviour required when the save button is
         clicked."""
 
-        wx.CallAfter(self.Destroy)
+        wx.CallAfter(self.__frame.Destroy)
