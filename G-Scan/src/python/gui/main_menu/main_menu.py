da2b31ee0b13069939d6a806ddb307d8f984fd7d
RMvirtual <rmvirtual@AOL.com>
2021-03-11 18:36:05 +0000

    Created a middle_panel module.

--- G-Scan/src/python/gui/main_menu/main_menu.py
+++ G-Scan/src/python/gui/main_menu/main_menu.py
@@ -2,6 +2,7 @@ import wx
 from app import file_system
 import date.date
 from gui.main_menu.top_panel import TopPanel
+from gui.main_menu.middle_panel import MiddlePanel
 import threading
 
 class MainMenu(wx.Frame):
@@ -58,28 +59,9 @@ class MainMenu(wx.Frame):
         """Creates the main panels for widgets to be instantiated in.
         For use with the __create_widgets() method."""
 
-        self.__create_top_panel()
-        self.__create_middle_panel()
-        self.__create_bottom_panel()
-
-    def __create_top_panel(self):
-        """Creates the top panel's sub-panels and corresponding
-        widgets."""
-
         self.__top_panel = TopPanel(self)
-
-    def __create_middle_panel(self):
-        """Creates the middle panel that contains the toolbar that runs
-        across the middle of the GUI in between the top and bottom
-        panels."""
-
-        self.__middle_panel = wx.Panel(
-            self,
-            size = (850, 30),
-            pos = (10, 265)
-        )
-
-        self.__create_toolbar_widgets()
+        self.__middle_panel = MiddlePanel(self)
+        self.__create_bottom_panel()
 
     def __create_bottom_panel(self):
         """Creates the bottom panel of GUI which contains the text
@@ -102,113 +84,7 @@ class MainMenu(wx.Frame):
         )
 
         self.__text_console_output_box.SetBackgroundColour("LIGHT GREY")
-
-    def __create_toolbar_widgets(self):
-        """Creates widgets related to the middle toolbar."""
-
-        # Start button.
-        self.__start_button = wx.Button(
-            self.__middle_panel,
-            label = "Start",
-            size = (60, 25),
-            pos = (0, 0)
-        )
-
-        self.__start_button.SetFont(self.__button_font)
-
-        self.__start_button.Bind(
-            wx.EVT_BUTTON,
-            self.__start_button_click
-        )
-
-        # Quick Mode user aid message for displaying a preview of the
-        # output that quick mode will calculate based on the current
-        # user settings and what they have entered so far in the user
-        # input entry box.
-        self.__quick_mode_preview_text = wx.StaticText(
-            self.__middle_panel,
-            label = "",
-            size = (180, 14),
-            pos = (155, 3),
-            style = wx.ALIGN_RIGHT
-        )
-
-        self.__quick_mode_preview_text.SetFont(wx.Font(
-            12, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u"calibri"))
-
-        # Michelin Man button.
-        michelin_man_logo_path = (
-            file_system.get_resources_directory() + "images\\michelin_logo.jpg")
-
-        michelin_man_logo = wx.Image(
-            michelin_man_logo_path, wx.BITMAP_TYPE_ANY).Scale(20, 20)
-        
-        self.__michelin_man_button = wx.BitmapButton(
-            self.__middle_panel,
-            bitmap = michelin_man_logo.ConvertToBitmap(),
-            size = (25, 25),
-            pos = (680, 0)
-        )
-
-        # Settings button.
-        self.__settings_button = wx.Button(
-            self.__middle_panel,
-            label = "Settings",
-            size = (60, 25),
-            pos = (710, 0)
-        )
-
-        self.__settings_button.SetFont(self.__button_font)
-
-        self.__settings_button.Bind(
-            wx.EVT_BUTTON,
-            self.__settings_button_click
-        )
-
-        # Exit button.
-        self.__exit_button = wx.Button(
-            self.__middle_panel,
-            label = "Exit",
-            size = (60, 25),
-            pos = (775, 0)
-        )
-
-        self.__exit_button.SetFont(self.__button_font)
-
-        self.__exit_button.Bind(
-            wx.EVT_BUTTON,
-            self.__exit_button_click
-        )
-
-    def __submit_button_click(self, event = None):
-        """Defines the behaviour to follow when the submit button
-        is clicked, activating the main application's submit
-        workflow method."""
-
-        print("Hello")
     
-    def __start_button_click(self, event = None):
-        """Defines the behavior to follow when the start button
-        is clicked on, activating the main application's start
-        workflow method."""
-
-        self.__main_application.start()
-
-    def __exit_button_click(self, event = None):
-        """Defines the behaviour to follow when the exit button
-        is clicked on, activating the main application's exit
-        workflow method."""
-
-        self.__main_application.exit()
-
-    def __settings_button_click(self, event = None):
-        """Defines the behaviour to follow when the exit button
-        is clicked on, activating the main application's exit
-        workflow method."""
-        
-        print("Settings button clicked.")
-        self.__main_application.open_settings_menu()
-
     def get_current_paperwork_type(self):
         """Gets the value of the paperwork type variable based on
         which button has been selected."""
@@ -252,4 +128,4 @@ class MainMenu(wx.Frame):
         """Overwrites the text found in the quick mode hint text
         box."""
         
-        self.__quick_mode_preview_text.SetLabel(text)
+        self.__middle_panel.set_quick_mode_hint_text(text)
---
+++ G-Scan/src/python/gui/main_menu/middle_panel.py
@@ -0,0 +1,139 @@
+import wx
+from app import file_system
+
+class MiddlePanel(wx.Panel):
+    """Middle panel for the main menu GUI."""
+
+    def __init__(self, frame):
+        """Creates a new middle panel."""
+
+        super().__init__(
+            frame,
+            size = (850, 30),
+            pos = (10, 265)
+        )
+
+        self.__frame = frame
+        self.__set_fonts()
+        self.__create_toolbar_widgets()
+        
+    def __set_fonts(self):
+        """Sets the fonts to be used for the widget types."""
+
+        self.__button_font = self.__create_font(11)
+        self.__body_font = self.__create_font(14)
+
+    def __create_font(self, font_size):
+        """Creates a calibri font to be used."""
+
+        font = wx.Font(
+            font_size, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u"calibri")
+
+        return font
+
+    def __create_toolbar_widgets(self):
+        """Creates widgets related to the middle toolbar."""
+
+        # Start button.
+        self.__start_button = wx.Button(
+            self,
+            label = "Start",
+            size = (60, 25),
+            pos = (0, 0)
+        )
+
+        self.__start_button.SetFont(self.__button_font)
+
+        self.__start_button.Bind(
+            wx.EVT_BUTTON,
+            self.__start_button_click
+        )
+
+        # Quick Mode user aid message for displaying a preview of the
+        # output that quick mode will calculate based on the current
+        # user settings and what they have entered so far in the user
+        # input entry box.
+        self.__quick_mode_preview_text = wx.StaticText(
+            self,
+            label = "",
+            size = (180, 14),
+            pos = (155, 3),
+            style = wx.ALIGN_RIGHT
+        )
+
+        self.__quick_mode_preview_text.SetFont(wx.Font(
+            12, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u"calibri"))
+
+        # Michelin Man button.
+        michelin_man_logo_path = (
+            file_system.get_resources_directory() + "images\\michelin_logo.jpg")
+
+        michelin_man_logo = wx.Image(
+            michelin_man_logo_path, wx.BITMAP_TYPE_ANY).Scale(20, 20)
+        
+        self.__michelin_man_button = wx.BitmapButton(
+            self,
+            bitmap = michelin_man_logo.ConvertToBitmap(),
+            size = (25, 25),
+            pos = (680, 0)
+        )
+
+        # Settings button.
+        self.__settings_button = wx.Button(
+            self,
+            label = "Settings",
+            size = (60, 25),
+            pos = (710, 0)
+        )
+
+        self.__settings_button.SetFont(self.__button_font)
+
+        self.__settings_button.Bind(
+            wx.EVT_BUTTON,
+            self.__settings_button_click
+        )
+
+        # Exit button.
+        self.__exit_button = wx.Button(
+            self,
+            label = "Exit",
+            size = (60, 25),
+            pos = (775, 0)
+        )
+
+        self.__exit_button.SetFont(self.__button_font)
+
+        self.__exit_button.Bind(
+            wx.EVT_BUTTON,
+            self.__exit_button_click
+        )
+
+    def __start_button_click(self, event = None):
+        """Defines the behavior to follow when the start button
+        is clicked on, activating the main application's start
+        workflow method."""
+
+        # self.__main_application.start()
+        print("Start button clicked.")
+
+    def __exit_button_click(self, event = None):
+        """Defines the behaviour to follow when the exit button
+        is clicked on, activating the main application's exit
+        workflow method."""
+
+        # self.__main_application.exit()
+        print("Exit button clicked.")
+
+    def __settings_button_click(self, event = None):
+        """Defines the behaviour to follow when the exit button
+        is clicked on, activating the main application's exit
+        workflow method."""
+        
+        print("Settings button clicked.")
+        # self.__main_application.open_settings_menu()
+
+    def set_quick_mode_hint_text(self, text):
+        """Overwrites the text found in the quick mode hint text
+        box."""
+        
+        self.__quick_mode_preview_text.SetLabel(text)
