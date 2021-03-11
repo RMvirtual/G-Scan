89fad699339d338d56033507f7126cb9b68b6307
RMvirtual <rmvirtual@AOL.com>
2021-03-11 18:52:25 +0000

    Created a base Panel class and extended BottomPanel from it.

--- G-Scan/src/python/gui/main_menu/bottom_panel.py
+++ G-Scan/src/python/gui/main_menu/bottom_panel.py
@@ -1,7 +1,7 @@
 import wx
-from app import file_system
+from gui.main_menu.panel import Panel
 
-class BottomPanel(wx.Panel):
+class BottomPanel(Panel):
     """Bottom panel for the main menu GUI."""
 
     def __init__(self, frame):
@@ -10,13 +10,14 @@ class BottomPanel(wx.Panel):
         super().__init__(
             frame,
             size = (840, 230),
-            pos = (10, 295)
+            position = (10, 295)
         )
 
-        self.__frame = frame
-        self.__set_fonts()
+        self.__create_text_console()
 
-        # Text console display.
+    def __create_text_console(self):
+        """Creates a text console display output box."""
+
         self.__text_console_output_box = wx.TextCtrl(
             self,
             size = (835, 230),
@@ -25,20 +26,6 @@ class BottomPanel(wx.Panel):
         )
 
         self.__text_console_output_box.SetBackgroundColour("LIGHT GREY")
-
-    def __set_fonts(self):
-        """Sets the fonts to be used for the widget types."""
-
-        self.__button_font = self.__create_font(11)
-        self.__body_font = self.__create_font(14)
-
-    def __create_font(self, font_size):
-        """Creates a calibri font to be used."""
-
-        font = wx.Font(
-            font_size, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u"calibri")
-
-        return font
 
     def write_log(self, text):
         """Writes a string of text to the console output log."""
---
+++ G-Scan/src/python/gui/main_menu/panel.py
@@ -0,0 +1,32 @@
+import wx
+
+class Panel(wx.Panel):
+    """A basic GUI panel (to be extended further into more concrete
+    panels).
+    """
+
+    def __init__(self, frame, size, position):
+        """Creates a new panel and attaches it to a frame."""
+
+        super().__init__(
+            frame,
+            size = size,
+            pos = position
+        )
+
+        self.__frame = frame
+        self.__set_fonts()
+
+    def __set_fonts(self):
+        """Sets the fonts to be used for the widget types."""
+
+        self.__button_font = self.__create_font(11)
+        self.__body_font = self.__create_font(14)
+
+    def __create_font(self, font_size):
+        """Creates a calibri font to be used."""
+
+        font = wx.Font(
+            font_size, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u"calibri")
+
+        return font
