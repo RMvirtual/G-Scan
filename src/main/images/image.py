1b00057b9ded1e17eb2c36327c0baefeaa5294dc
RMvirtual <rmvirtual@AOL.com>
2022-03-31 17:48:43 +0100

    Refactored image rotation function.

--- src/main/images/image.py
+++ src/main/images/image.py
@@ -2,10 +2,8 @@ import wand.image
 
 
 def rotate_to_portrait(source: str, output: str) -> None:
-    with wand.image.Image(
-            filename=source, resolution=300) as img_simulator:
-        if img_simulator.width > img_simulator.height:
-            img_simulator.rotate(270)
-            img_simulator.save(filename=output)
-        else:
-            img_simulator.save(filename=output)
+    with wand.image.Image(filename=source, resolution=300) as image_stream:
+        if image_stream.width > image_stream.height:
+            image_stream.rotate(270)
+
+        image_stream.save(filename=output)
