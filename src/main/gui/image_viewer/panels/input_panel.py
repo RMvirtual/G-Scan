680854fc05885281658a396e79d0fb117af412ac
RMvirtual <rmvirtual@AOL.com>
2022-04-01 15:31:31 +0100

    Added three buttons.

--- src/main/gui/image_viewer/panels/input_panel.py
+++ src/main/gui/image_viewer/panels/input_panel.py
@@ -11,10 +11,16 @@ class InputPanel(wx.Panel):
             parent=self, label="Please enter job reference:")
 
         self._job_reference_input = wx.TextCtrl(parent=self)
+        self._submit_button = wx.Button(parent=self, label="Submit")
+        self._skip_button = wx.Button(parent=self, label="Skip")
+        self._split_button = wx.Button(parent=self, label="Split")
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self._reference_input_label, 0, wx.ALIGN_CENTRE_HORIZONTAL)
         sizer.Add(self._job_reference_input, 0, wx.ALIGN_CENTRE_HORIZONTAL)
+        sizer.Add(self._submit_button, 0, wx.ALIGN_CENTRE_HORIZONTAL)
+        sizer.Add(self._skip_button, 0, wx.ALIGN_CENTRE_HORIZONTAL)
+        sizer.Add(self._split_button, 0, wx.ALIGN_CENTRE_HORIZONTAL)
 
         sizer.SetSizeHints(self)
         self.SetSizer(sizer)
