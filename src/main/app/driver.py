503c52218cf9f09589e9200e6160817b489ed4ab
RMvirtual <rmvirtual@AOL.com>
2022-04-01 15:08:38 +0100

    Added sizer to job reference input widgets on image viewer.

--- src/main/app/driver.py
+++ src/main/app/driver.py
@@ -1,12 +1,13 @@
 from src.main.gui.app.app import GuiApplication
 from src.main.gui.main_menu.main_menu import MainMenu
 from src.main.gui.settings.settings_menu import SettingsMenu
-
+from src.main.gui.image_viewer.viewer import ImageViewer
 
 def main():
     gui_app = GuiApplication()
-    _main_menu = MainMenu()
-    _settings_menu = SettingsMenu()
+    # _main_menu = MainMenu()
+    # _settings_menu = SettingsMenu()
+    _image_viewer = ImageViewer((200, 200))
     gui_app.run()
 
 
--- src/main/gui/image_viewer/viewer.py
+++ src/main/gui/image_viewer/viewer.py
@@ -1,11 +1,27 @@
 from src.main.gui.widgets.frame import Frame
+import wx
 
 
 class ImageViewer(Frame):
     def __init__(self, size):
         super().__init__(size=size, title="Paperwork Viewer")
         self._initialise_widgets()
+        self.Show()
 
     def _initialise_widgets(self) -> None:
         self.SetBackgroundColour("WHITE")
 
+        self._input_panel = wx.Panel(parent=self)
+
+        self._reference_input_label = wx.StaticText(
+            parent=self._input_panel, label="Please enter job reference:")
+
+        self._job_reference_input = wx.TextCtrl(parent=self._input_panel)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self._reference_input_label)
+        sizer.Add(self._job_reference_input)
+
+        sizer.SetSizeHints(self._input_panel)
+        self._input_panel.SetSizer(sizer)
+        self.SetSizer(sizer)
