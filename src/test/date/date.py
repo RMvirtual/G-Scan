346e7f73169d6f209c6c82aa4a1785e61d499846
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-06-21 18:08:11 +0100

    Further work tidying up date tests. Constants added.

--- src/test/date/date.py
+++ src/test/date/date.py
@@ -5,27 +5,27 @@ import src.main.date.calendar as calendar
 class TestDate(unittest.TestCase):
     """A class for testing the date module."""
 
-    CORRECT_MONTH_NAMES = [
+    CORRECT_MONTH_NAMES = (
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
-    ]
+    )
 
-    CORRECT_MONTH_NAME_AND_NUMBERS = [
+    CORRECT_MONTH_NAME_AND_NUMBERS = (
         "January - 01", "February - 02", "March - 03",
         "April - 04", "May - 05", "June - 06",
         "July - 07", "August - 08", "September - 09",
         "October - 10", "November - 11", "December - 12"
-    ]
+    )
 
     def test_should_get_month_names(self):
-        months = calendar.months(2021)
-
-        for month in months:
+        for month in calendar.months(2021):
             self.assertTrue(month.month_name() in self.CORRECT_MONTH_NAMES)
 
     def test_should_get_years_from_months(self):
-        for month in calendar.months(2021):
-            self.assertTrue(month.year() == 2021)
+        year = 2021
+
+        for month in calendar.months(year):
+            self.assertTrue(month.year() == year)
 
     def test_should_get_month_names_and_numbers(self):
         months = calendar.month_names_and_numbers()
