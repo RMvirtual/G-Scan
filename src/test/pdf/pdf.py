f76c9cef8e79c2124970dcf194203a9ae28443d3
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-06-24 17:40:16 +0100

    Removed failing tests; back to basics.

--- src/test/pdf/pdf.py
+++ src/test/pdf/pdf.py
@@ -5,6 +5,10 @@ import src.main.pdf.reader as pdf_reader
 
 
 class TestPdfReader(unittest.TestCase):
+    DOCUMENT_HEIGHT = 300
+    DOCUMENT_WIDTH = 300
+    PIXEL_COLOURS = 3
+
     def test_should_read_300x300_pdf(self):
         self._populate_correct_pdf_values()
 
@@ -15,37 +19,32 @@ class TestPdfReader(unittest.TestCase):
 
         page = pdf_reader.read_pdf(source=pdf_path)[0]
 
-        dpi_300_pixel_dims = {
-            "height": 300,
-            "width": 300
-        }
+        self.assertEqual(self.DOCUMENT_HEIGHT, page.height)
+        self.assertEqual(self.DOCUMENT_WIDTH, page.width)
 
-        self.assertEqual(dpi_300_pixel_dims["height"], page.height)
-        self.assertEqual(dpi_300_pixel_dims["width"], page.width)
-
-        for row in range(dpi_300_pixel_dims["height"]):
-            for column in range(dpi_300_pixel_dims["width"]):
+        for row in range(self.DOCUMENT_HEIGHT):
+            for column in range(self.DOCUMENT_WIDTH):
                 pixel = page.pixel(row, column)
                 correct_pixel = self.CORRECT_PDF_VALUES[column, row]
 
-                for i in range(3):
-                    self.assertEqual(pixel[i], correct_pixel[i], msg=(
-                        "\nRow: " + str(row) + ", Column: " + str(column)
-                    ))
+                for pixel_no in range(self.PIXEL_COLOURS):
+                    self.assertEqual(
+                        pixel[pixel_no], correct_pixel[pixel_no],
+                        msg=("\nRow: " + str(row) + ", Column: " + str(column))
+                    )
 
     def _populate_correct_pdf_values(self):
-        black_pixels = numpy.full(
-            shape=(150, 300, 3),
-            fill_value=(255, 255, 255)
-        )
-
-        white_pixels = numpy.full(
-            shape=(150, 300, 3),
-            fill_value=(0, 0, 0)
-        )
+        black_pixels = self._150x300_matrix((255, 255, 255))
+        white_pixels = self._150x300_matrix((0, 0, 0))
 
         self.CORRECT_PDF_VALUES = numpy.concatenate(
             (white_pixels, black_pixels), axis=0)
+
+    def _150x300_matrix(self, fill_value):
+        return numpy.full(
+            shape=(150, 300, 3),
+            fill_value=fill_value
+        )
 
 
 if __name__ == "__main__":
