093cc0628d987ed260839851166c18952ea95e2b
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-24 16:51:30 +0000

    Increased Button size on initial menu.

--- src/main/gui/menu/initial_options.py
+++ src/main/gui/menu/initial_options.py
@@ -4,11 +4,14 @@ class InitialOptions(wx.Panel):
     def __init__(self, parent: wx.Frame):
         super(InitialOptions, self).__init__(parent)
 
+        font = wx.Font(wx.FontInfo(pointSize=30).Bold())
         self.ops = wx.Button(parent=self, label="Ops")
+        self.ops.SetFont(font)
+
         self.pods = wx.Button(parent=self, label="PODs")
+        self.pods.SetFont(font)
 
         self.sizer = wx.BoxSizer(orient=wx.HORIZONTAL)
-
         self.sizer.AddStretchSpacer(prop=1)
 
         self.sizer.Add(
