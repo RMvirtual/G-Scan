cd234b995a03bc307e293ac3a3479e3a7a7fce86
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-26 13:23:26 +0000

    Moved callback logic into controller class for main menu.

--- src/main/app/main_menu/controller.py
+++ src/main/app/main_menu/controller.py
@@ -1,7 +1,26 @@
+import wx
 from src.main.gui.menu.main import MainMenu
 
 
 class MainMenuController:
     def __init__(self):
         self._menu = MainMenu("G-Scan")
+        self._initialise_callbacks()
         self._menu.Show()
+
+    def _initialise_callbacks(self) -> None:
+        self._menu.departments.ops.Bind(wx.EVT_BUTTON, self._menu.view_ops)
+
+        self._menu.operations.back.Bind(
+            wx.EVT_BUTTON, self._menu.view_departments)
+
+        self._menu.departments.exit.Bind(wx.EVT_BUTTON, self._exit_btn_press)
+
+        self._menu.operations.cust_pwork.Bind(
+            wx.EVT_BUTTON, self._paperwork_viewer)
+
+    def _exit_btn_press(self, event = None) -> None:
+        self._menu.Close()
+
+    def _paperwork_viewer(self, event = None) -> None:
+        print("Paperwork Viewer should launch.")
--- src/main/gui/menu/main.py
+++ src/main/gui/menu/main.py
@@ -13,43 +13,33 @@ class MainMenu(wx.Frame):
 
         self._initialise_panels()
         self._initialise_sizer()
-        self._initialise_callbacks()
         self.SetBackgroundColour(colour=wx.WHITE)
 
     def _initialise_panels(self) -> None:
-        self._logo = Logo(self)
-        self._departments = Departments(self)
-        self._operations = OperationsOptions(self)
+        self.logo = Logo(self)
+        self.departments = Departments(self)
+        self.operations = OperationsOptions(self)
 
-        self._operations.Hide()
+        self.operations.Hide()
 
     def _initialise_sizer(self) -> None:
         sizer = wx.BoxSizer(orient=wx.VERTICAL)
         flags = wx.EXPAND
 
-        sizer.Add(window=self._logo, proportion=1, flag=flags, border=0)
-        sizer.Add(window=self._departments, proportion=2, flag=flags, border=0)
-        sizer.Add(window=self._operations, proportion=2, flag=flags, border=0)
+        sizer.Add(window=self.logo, proportion=1, flag=flags, border=0)
+        sizer.Add(window=self.departments, proportion=2, flag=flags, border=0)
+        sizer.Add(window=self.operations, proportion=2, flag=flags, border=0)
 
         self.SetSizer(sizer)
 
-    def _initialise_callbacks(self) -> None:
-        self._departments.ops.Bind(wx.EVT_BUTTON, self._view_ops)
-        self._departments.exit.Bind(wx.EVT_BUTTON, self._exit_btn_press)
-        self._operations.back.Bind(wx.EVT_BUTTON, self._view_departments)
-
-
-    def _view_ops(self, event = None) -> None:
-        self._departments.Hide()
-        self._operations.Show()
+    def view_ops(self, event = None) -> None:
+        self.departments.Hide()
+        self.operations.Show()
 
         self.Layout()
 
-    def _view_departments(self, event = None) -> None:
-        self._operations.Hide()
-        self._departments.Show()
+    def view_departments(self, event = None) -> None:
+        self.operations.Hide()
+        self.departments.Show()
 
         self.Layout()
-
-    def _exit_btn_press(self, event = None) -> None:
-        self.Close()
