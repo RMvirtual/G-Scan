26a0a2a50743c08f40f0db5acb1f0f73fbac69c0
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-27 10:48:21 +0000

    More work on interactive GUI with main window frame.

--- src/main/app/controllers/application.py
+++ src/main/app/controllers/application.py
@@ -7,6 +7,7 @@ from src.main.app.controllers.window import WindowController
 class ApplicationController:
     def __init__(self):
         self._initialise_window()
+
         self.active_controller = None
 
     def _initialise_window(self) -> None:
@@ -17,5 +18,20 @@ class ApplicationController:
         self.active_controller = MainMenuController(
             self._window_controller.window)
 
+        self.active_controller.bind_customer_paperwork(self.launch_image_viewer)
+        self.active_controller.bind_loading_list(self.launch_image_viewer)
+
+        self.active_controller.bind_settings(self.launch_settings)
+        self.active_controller.bind_exit(self.close)
+
         self._window_controller.set_panel(self.active_controller.panel)
         self.active_controller.show()
+
+    def launch_image_viewer(self, event = None) -> None:
+        print("Image Viewer launch.")
+
+    def launch_settings(self, event = None) -> None:
+        print("Settings launch.")
+
+    def close(self, event = None) -> None:
+        self._window_controller.close()
