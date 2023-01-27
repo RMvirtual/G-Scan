eacac74a5edcf01d94c2391b298ee7fad4c7c9ca
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-27 12:44:47 +0000

    Refactored into display method.

--- src/main/app/controllers/application.py
+++ src/main/app/controllers/application.py
@@ -14,38 +14,26 @@ class ApplicationController:
         self._window.show()
 
     def launch_main_menu(self, event = None) -> None:
-        if self.active_controller:
-            self.active_controller.close()
-
         main_menu = MainMenuController(self._window)
         main_menu.bind_customer_paperwork(self.launch_image_viewer)
         main_menu.bind_loading_list(self.launch_image_viewer)
         main_menu.bind_settings(self.launch_settings)
         main_menu.bind_exit(self.close)
 
-        self._window.display(main_menu)
-        self.active_controller = main_menu
+        self.display(main_menu)
 
     def launch_image_viewer(self, event = None) -> None:
-        if self.active_controller:
-            self.active_controller.close()
-
         image_viewer = ImageViewerController(self._window)
         image_viewer.bind_exit(self.launch_main_menu)
-        self._window.display(image_viewer)
 
-        self.active_controller = image_viewer
+        self.display(image_viewer)
 
     def launch_settings(self, event = None) -> None:
-        if self.active_controller:
-            self.active_controller.close()
-
         settings = SettingsController(self._window)
-        self._window.display(settings)
         settings.bind_save_button(self.save_settings)
         settings.bind_exit_button(self.launch_main_menu)
 
-        self.active_controller = settings
+        self.display(settings)
 
     def close(self, event = None) -> None:
         self._window.close()
@@ -53,8 +41,9 @@ class ApplicationController:
     def save_settings(self, event = None) -> None:
         self.launch_main_menu()
 
-    def _set_controller(self, controller: DisplayController) -> None:
+    def display(self, controller: DisplayController) -> None:
         if self.active_controller:
             self.active_controller.close()
 
         self.active_controller = controller
+        self._window.display(self.active_controller)
