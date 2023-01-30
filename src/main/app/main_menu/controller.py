0819114c1e4c7e3d1d7830ba089331930ee00b99
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-30 15:20:49 +0000

    More tidying of the main menu controller.

--- src/main/app/main_menu/controller.py
+++ src/main/app/main_menu/controller.py
@@ -20,10 +20,16 @@ class MainMenuController:
         self._bind_root_application_callbacks()
 
     def _bind_root_application_callbacks(self) -> None:
-        self.bind_customer_paperwork(self._app.launch_image_viewer)
-        self.bind_loading_list(self._app.launch_image_viewer)
-        self.bind_settings(self._app.launch_settings)
-        self.bind_exit(self._app.exit)
+        self._menu.departments.toolbar.exit.Bind(wx.EVT_BUTTON, self.on_exit)
+
+        self._menu.departments.toolbar.settings.Bind(
+            wx.EVT_BUTTON, self.on_settings)
+
+        self._menu.operations.options.cust_pwork.Bind(
+            wx.EVT_BUTTON, self.on_customer_paperwork)
+
+        self._menu.operations.options.loading_list.Bind(
+            wx.EVT_BUTTON, self.on_loading_list)
 
     @property
     def panel(self) -> wx.Panel:
@@ -38,14 +44,14 @@ class MainMenuController:
     def close(self) -> None:
         self._menu.Close()
 
-    def bind_exit(self, callback) -> None:
-        self._menu.departments.toolbar.exit.Bind(wx.EVT_BUTTON, callback)
+    def on_exit(self, event = None) -> None:
+        self._app.exit()
 
-    def bind_customer_paperwork(self, callback) -> None:
-        self._menu.operations.options.cust_pwork.Bind(wx.EVT_BUTTON, callback)
+    def on_customer_paperwork(self, event = None) -> None:
+        self._app.launch_image_viewer()
 
-    def bind_loading_list(self, callback) -> None:
-        self._menu.operations.options.loading_list.Bind(wx.EVT_BUTTON, callback)
+    def on_loading_list(self, event = None) -> None:
+        self._app.launch_image_viewer()
 
-    def bind_settings(self, callback) -> None:
-        self._menu.departments.toolbar.settings.Bind(wx.EVT_BUTTON, callback)
+    def on_settings(self, event = None) -> None:
+        self._app.launch_settings()
