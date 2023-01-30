a2482a5f3be0af5a96dbd797b0ecec6210490d83
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-30 09:57:28 +0000

    Added title to files panel.

--- src/main/gui/image_viewer/files.py
+++ src/main/gui/image_viewer/files.py
@@ -4,4 +4,15 @@ import wx
 class FilesPanel(wx.Panel):
     def __init__(self, parent: wx.Window):
         super(FilesPanel, self).__init__(parent=parent)
+        self._initialise_widgets()
+        self._initialise_sizer()
 
+    def _initialise_widgets(self) -> None:
+        self._title = wx.StaticText(parent=self, label="Jobs")
+        font = wx.Font(wx.FontInfo(pointSize=30).Bold())
+        self._title.SetFont(font)
+
+    def _initialise_sizer(self) -> None:
+        sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        sizer.Add(window=self._title, proportion=0, flag=wx.EXPAND, border=5)
+        self.SetSizer(sizer)
