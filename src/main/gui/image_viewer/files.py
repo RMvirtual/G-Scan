4f3a01a50932d99cfcb80ef493c1133109f69fd8
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-30 09:30:09 +0000

    Started file panel in the image viewer.

---
+++ src/main/gui/image_viewer/files.py
@@ -0,0 +1,7 @@
+import wx
+
+
+class FilesPanel(wx.Panel):
+    def __init__(self, parent: wx.Window):
+        super(FilesPanel, self).__init__(parent=parent)
+
--- src/main/gui/image_viewer/menu_bar.py
+++
@@ -1,6 +0,0 @@
-import wx
-
-
-class MenuBar(wx.MenuBar):
-    def __init__(self) -> None:
-        super().__init__()
