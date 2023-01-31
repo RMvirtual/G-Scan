557627a0cf2f0afbf59bf32e397754bc22e9875d
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-31 16:34:03 +0000

    Deleted deprecated widgets module.

--- src/main/gui/widgets/frame.py
+++
@@ -1,7 +0,0 @@
-from wx import Frame as wxFrame
-
-
-class Frame(wxFrame):
-    def __init__(self, size, title):
-        super().__init__(None, size=size, title=title)
-        self.SetBackgroundColour("WHITE")
