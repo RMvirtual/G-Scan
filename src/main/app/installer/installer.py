893eb5e14a65ebe3e0553e96df5b3c6855c28a89
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-31 10:21:16 +0000

    Refactored installer set up.

--- src/main/app/installer/installer.py
+++ src/main/app/installer/installer.py
@@ -3,19 +3,32 @@ import shutil
 
 
 def install_app_data() -> None:
-    app_data_path = os.environ["LOCALAPPDATA"] + "\\G-Scan"
+    make_app_data_directory()
+    empty_app_data()
 
-    if not os.path.exists(app_data_path):
+
+def make_app_data_directory():
+    if not os.path.exists(app_data_path()):
         os.mkdir(app_data_path)
 
-    for filename in os.listdir(app_data_path):
+
+def empty_app_data():
+    for filename in os.listdir(app_data_path()):
         file_path = os.path.join(app_data_path, filename)
+        delete_file(file_path)
 
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
 
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+def delete_file(file_path):
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+
+    except Exception as e:
+        print(f"Failed to delete {file_path}. Reason: {e}")
+
+
+def app_data_path() -> str:
+    return os.environ["LOCALAPPDATA"] + "\\G-Scan"
