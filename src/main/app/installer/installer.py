029883c6e9a71f36a2114faa71d6c18a1d2d1a9c
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-01-31 10:10:43 +0000

    Installer script empties directory to ensure clean install.

--- src/main/app/installer/installer.py
+++ src/main/app/installer/installer.py
@@ -1,10 +1,18 @@
 import os
+import shutil
 
 
 def install_app_data() -> None:
     app_data_path = os.environ["LOCALAPPDATA"] + "\\G-Scan"
 
-    exists = False
-
-    if not exists:
+    if not os.path.exists(app_data_path):
         os.mkdir(app_data_path)
+
+    for filename in os.listdir(app_data_path):
+        filepath = os.path.join(app_data_path, filename)
+
+        try:
+            shutil.rmtree(filepath)
+
+        except OSError:
+            os.remove(filepath)
