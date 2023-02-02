191360a687bfa4949aae67d327a8cacafe9912d1
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-02-02 09:21:00 +0000

    Menu bar on window removes dynamically.

--- src/main/app/controllers/viewer.py
+++ src/main/app/controllers/viewer.py
@@ -20,7 +20,16 @@ class ImageViewerController:
 
     def _initialise_gui(self) -> None:
         self._gui = ImageViewer(self._root.window)
-        self._root.window.set_panel(self._gui)
+
+        window = self._root.window
+        window.set_panel(self._gui)
+
+        menu_bar = wx.MenuBar()
+        file_menu = wx.Menu()
+        file_item = file_menu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
+        menu_bar.Append(file_menu, '&File')
+
+        window.SetMenuBar(menu_bar)
 
     def _initialise_callbacks(self) -> None:
         self._gui.bottom_toolbar.exit.Bind(wx.EVT_BUTTON, self.on_exit)
@@ -30,8 +39,13 @@ class ImageViewerController:
         self._exit_to_main_menu()
 
     def on_close(self, event = None) -> None:
+        self._tear_down_gui()
+
+    def _tear_down_gui(self) -> None:
         self._root.window.Freeze()
         self._gui.Destroy()
+        window = self._root.window
+        window.SetMenuBar(wx.MenuBar())
 
     def _exit_to_main_menu(self) -> None:
         self._gui.Close()
