2addb6b7a7320973e90f9e6b49448ff569a5b4ce
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-02-03 15:48:32 +0000

    More refactoring of user toolbar.

--- src/main/gui/image_viewer/toolbars/user_toolbar.py
+++ src/main/gui/image_viewer/toolbars/user_toolbar.py
@@ -17,11 +17,14 @@ class UserToolbar(wx.Panel):
         self._initialise_buttons()
 
     def _initialise_input_box(self) -> None:
-        self._input_label = wx.StaticText(
-            self, label="Please enter job reference:")
+        font = fonts.font(point_size=11)
 
-        self._input_label.SetFont(fonts.font(point_size=11))
+        label_text = "Please enter job reference:"
+        self._input_label = wx.StaticText(self, label=label_text)
+        self._input_label.SetFont(font)
+
         self.reference_input = wx.TextCtrl(self)
+        self.reference_input.SetFont(font)
 
     def _initialise_dropdown_boxes(self) -> None:
         font = fonts.font(point_size=9, bold=True)
@@ -30,9 +33,9 @@ class UserToolbar(wx.Panel):
         self._department_label.SetFont(font)
         self.department_box = self._default_combobox()
 
-        self._document_type_label = wx.StaticText(self, label="Document Type")
-        self._document_type_label.SetFont(font)
-        self.document_type_box = self._default_combobox()
+        self._document_label = wx.StaticText(self, label="Document Type")
+        self._document_label.SetFont(font)
+        self.document_box = self._default_combobox()
 
     def _initialise_buttons(self) -> None:
         self.submit = wx.Button(self, label="Submit")
@@ -43,37 +46,36 @@ class UserToolbar(wx.Panel):
         sizer = wx.GridBagSizer(vgap=0, hgap=0)
 
         border = 5
-        row_0_flags = wx.LEFT|wx.RIGHT|wx.ALIGN_BOTTOM
+        label_flags = wx.LEFT|wx.RIGHT|wx.ALIGN_BOTTOM
 
         sizer.Add(
-            window=self._input_label, pos=(0,0), span=(1,4),
-            flag=wx.TOP|row_0_flags, border=border
+            window=self._input_label,
+            pos=(0,0), span=(1,4), flag=wx.TOP|label_flags, border=border
         )
 
         sizer.Add(
-            window=self._department_label, pos=(0,4),
-            flag=row_0_flags, border=border
+            window=self._department_label,
+            pos=(0,4), flag=label_flags, border=border
         )
 
         sizer.Add(
-            window=self._document_type_label, pos=(0,5),
-            flag=row_0_flags, border=border
+            window=self._document_label,
+            pos=(0,5), flag=label_flags, border=border
         )
 
+        flags = wx.ALL
         sizer.Add(
-            window=self.reference_input, pos=(1, 0), flag=wx.ALL, border=border)
+            window=self.reference_input, pos=(1, 0), flag=flags, border=border)
 
-        sizer.Add(window=self.submit, pos=(1,1), flag=wx.ALL, border=border)
-        sizer.Add(window=self.skip, pos=(1,2), flag=wx.ALL, border=border)
-        sizer.Add(window=self.split, pos=(1,3), flag=wx.ALL, border=border)
-
-        sizer.Add(
-            window=self.department_box, pos=(1, 4), flag=wx.ALL, border=border)
+        sizer.Add(window=self.submit, pos=(1,1), flag=flags, border=border)
+        sizer.Add(window=self.skip, pos=(1,2), flag=flags, border=border)
+        sizer.Add(window=self.split, pos=(1,3), flag=flags, border=border)
 
         sizer.Add(
-            window=self.document_type_box,
-            pos=(1, 5), flag=wx.ALL, border=border
-        )
+            window=self.department_box, pos=(1, 4), flag=flags, border=border)
+
+        sizer.Add(
+            window=self.document_box, pos=(1, 5), flag=flags, border=border)
 
         self.SetSizer(sizer)
 
@@ -102,19 +104,19 @@ class UserToolbar(wx.Panel):
 
     @property
     def document_type(self) -> str:
-        return self.document_type_box.GetValue()
+        return self.document_box.GetValue()
 
     @document_type.setter
     def document_type(self, new_document_type: str) -> None:
-        self.document_type_box.SetValue(new_document_type)
+        self.document_box.SetValue(new_document_type)
 
     @property
     def document_options(self) -> list[str]:
-        return self.document_type_box.GetItems()
+        return self.document_box.GetItems()
 
     @document_options.setter
     def document_options(self, new_options: list[str]) -> None:
-        self.document_type_box.SetItems(new_options)
+        self.document_box.SetItems(new_options)
         self.Layout()
 
     def _default_combobox(self) -> wx.ComboBox:
