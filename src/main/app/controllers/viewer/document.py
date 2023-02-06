d7293932b02e5bad16eb1aad2660980c5473c1c0
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-02-06 13:48:37 +0000

    More refactoring.

--- src/main/app/controllers/viewer/document.py
+++ src/main/app/controllers/viewer/document.py
@@ -11,7 +11,6 @@ class DocumentController:
     def __init__(self, gui: ImageViewer):
         self._gui = gui
         self._page_view = PageViewController(page_canvas=self._gui.page_view)
-        self.pending = PendingDocuments(self._gui.file_tree.tree)
         self.document_tree = DocumentTree(self._gui.file_tree.tree)
 
         self._bind_callbacks()
@@ -37,18 +36,14 @@ class DocumentController:
     def import_files(self):
         files = file_system.request_files_to_import()
 
-        if not files:
-            return
-
-        self._process_files(files)
+        if files:
+            self._process_files(files)
 
     def import_as(self) -> None:
         print("Michelin Mode")
 
     def add_pending_files(self, paths: list[str]) -> list[PendingDocument]:
-        result = [self.add_pending_file(path) for path in paths]
-
-        return result
+        return [self.add_pending_file(path) for path in paths]
 
     def add_pending_file(self, path: str) -> PendingDocument:
         return self.document_tree.add_pending(file_path=path)
