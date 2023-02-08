af98c0e75cc6a163579fda816a0874c87110184d
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2023-02-08 15:05:26 +0000

    Moved constructor docstring.

--- src/main/documents/trees/tree.py
+++ src/main/documents/trees/tree.py
@@ -6,10 +6,10 @@ from src.main.documents.trees.processed import (
 
 
 class DocumentTree(AbstractRoot):
-    """Requires a wx.TreeCtrl object to plug into to create the node
-    references.
-    """
     def __init__(self, tree_control: wx.TreeCtrl) -> None:
+        """Requires a wx.TreeCtrl object to plug into to create the node
+        references.
+        """
         super().__init__(tree_control=tree_control)
 
         self.pending_branch = PendingBranch(parent=self)
