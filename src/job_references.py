8757f83a01ead5d205af5931daf2d641adca305c
RMvirtual <rmvirtual@AOL.com>
2023-12-07 20:56:49 +0000

    More refactoring on job references.

--- src/job_references.py
+++ src/job_references.py
@@ -3,38 +3,23 @@ from date import Date
 
 
 class GrReference:
-    """GR + 9 digits. First 4 digits: yymm; last 5 digits: job no."""
+    def __init__(self, date: Date = None, job_number: str = None) -> None:
+        """GR + 9 digits. First 4 digits: yymm; last 5 digits: job no."""
 
-    def __init__(self, date: Date = None, job_number: str = None):
+        if not (date or job_number) or (date and job_number):
+            raise ValueError("Must pass one parameter: date or job number.")
+
         self.prefix = "GR"
 
         if job_number:
-            if date:
-                pass  # Not thought this out when both params used.
-
-            else:
-                self._set_full_job_number(job_number)
+            self._set_full_job_number(job_number)
 
         else:
-            if date:
-                self._date = date
-
-            else:
-                self._date = calendar.current_month()
-
+            self._date = date
             self._job_number = "00000"
 
     def __str__(self) -> str:
         return self.prefix + self._date.format("yymm") + self._job_number
-
-    def _set_full_job_number(self, job_number: str) -> None:
-        digits = self._clean_job_number_candidate(job_number)
-
-        if not len(digits) == 9 and digits.isnumeric():
-            raise ValueError(f"Incorrect number of digits in {digits}.")
-
-        self._date = calendar.date(int(digits[2:4]), int(digits[0:2]))
-        self._job_number = digits[-5:]
 
     @property
     def job_number(self) -> str:
@@ -50,5 +35,15 @@ class GrReference:
 
         self._job_number = "0" * (5-len(digits)) + digits
 
+    def _set_full_job_number(self, job_number: str) -> None:
+        digits = self._clean_job_number_candidate(job_number)
+
+        if not len(digits) == 9 and digits.isnumeric():
+            raise ValueError(f"Incorrect job reference format: {job_number}.")
+
+        self._date = calendar.date(int(digits[2:4]), int(digits[0:2]))
+        self._job_number = digits[-5:]
+
     def _clean_job_number_candidate(self, job_number: str) -> str:
         return job_number.lower().removeprefix(self.prefix.lower()).strip()
+    
