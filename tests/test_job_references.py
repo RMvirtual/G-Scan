c1e4e00e00bd1b662a443f1e85b12dfd6991c9a5
RMvirtual <rmvirtual@AOL.com>
2023-12-07 19:25:29 +0000

    Fix test runner path import.

--- tests/test_job_references.py
+++ tests/test_job_references.py
@@ -1,6 +1,10 @@
 from date.date import Date
+from job_references.references import GrReference
 
 
-def test_should_fail() -> None:
-    assert False
+def test_should_produce_full_gr_representation() -> None:
+    date = Date(12, "December", 2023)
+    result = GrReference(date, job_number="01234")
+
+    assert result.job_number == "GR231201234"
 
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -5,8 +5,11 @@ from pathlib import Path
 
 
 def main() -> None:
-    binaries = Path(f"{__file__}/../release/gscan/bin")
+    binaries = Path(f"{__file__}/../../release/gscan/bin").resolve()
     sys.path.append((str(binaries)))
+
+    for path in sys.path:
+        print(path)
 
     arguments = ["--ignore=test_runner.py"]
     retcode = pytest.main(arguments)
