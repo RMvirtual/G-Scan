82b62f31e8097e45e0bd71087a9a4d322f6f30cc
RMvirtual <rmvirtual@AOL.com>
2023-12-22 21:31:27 +0000

    Partial work on sizer refactoring in settings.

--- src/gui/settings/settings.py
+++ src/gui/settings/settings.py
@@ -29,18 +29,34 @@ class Settings(wx.Panel):
     def _initialise_sizer(self) -> None:
         sizer = wx.BoxSizer(orient=wx.VERTICAL)
 
-        flags = wx.ALIGN_LEFT|wx.ALL
-        sizer.Add(window=self.title, proportion=0, flag=flags, border=15)
+        widget_to_flags = {
+            self.title: wx.ALIGN_LEFT|wx.ALL,
+            self.directories: wx.EXPAND|wx.ALL,
+            self.defaults: wx.ALIGN_LEFT|wx.ALL,
+            self.save: wx.ALIGN_RIGHT|wx.RIGHT|wx.BOTTOM,
+            self.exit: wx.ALIGN_RIGHT|wx.RIGHT|wx.BOTTOM
+        }
 
-        flags = wx.EXPAND|wx.ALL
-        sizer.Add(window=self.directories, proportion=0, flag=flags, border=15)
+        sizer.Add(
+            window=self.title, proportion=0, flag=wx.ALIGN_LEFT|wx.ALL, 
+            border=15
+        )
 
-        flags = wx.ALIGN_LEFT|wx.ALL
-        sizer.Add(window=self.defaults, proportion=0, flag=flags, border=15)
+        sizer.Add(
+            window=self.directories, proportion=0, flag=wx.EXPAND|wx.ALL, 
+            border=15
+        )
 
-        flags = wx.ALIGN_RIGHT|wx.RIGHT|wx.BOTTOM
-        sizer.Add(window=self.save, proportion=0, flag=flags, border=15)
-        sizer.Add(window=self.exit, proportion=0, flag=flags, border=15)
+        sizer.Add(
+            window=self.defaults, proportion=0, flag=wx.ALIGN_LEFT|wx.ALL,
+            border=15
+        )
+
+        for button in (self.save, self.exit):
+            sizer.Add(
+                window=button, proportion=0, 
+                flag=wx.ALIGN_RIGHT|wx.RIGHT|wx.BOTTOM, border=15
+            )
 
         sizer.AddStretchSpacer()
         self.SetSizer(sizer)
