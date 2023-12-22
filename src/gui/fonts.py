23eaba5e29298ba8cf4fa23ed469b4e6d6357ed0
RMvirtual <rmvirtual@AOL.com>
2023-12-22 22:30:11 +0000

    Inline font wrapping.

--- src/gui/fonts.py
+++ src/gui/fonts.py
@@ -2,13 +2,9 @@ import wx
 
 
 def font(point_size: int, bold: bool = False) -> wx.Font:
-    return wx.Font(_font_info(point_size, bold))
-
-
-def _font_info(point_size: int, bold: bool = False) -> wx.FontInfo:
     result = wx.FontInfo(pointSize=point_size)
 
     if bold:
         result.Bold()
 
-    return result
+    return wx.Font(result)
--- src/gui/window.py
+++ src/gui/window.py
@@ -6,34 +6,23 @@ from gui.metrics import recommended_metrics
 class Window(wx.Frame):
     def __init__(self):
         size, position = recommended_metrics()
-        super().__init__(parent=None, title="", size=size, pos=position)
+        super().__init__(None, title="", size=size, pos=position)
 
-        self._initialise_widgets()
-
-    def _initialise_widgets(self) -> None:
         self._panel = None
         self.SetBackgroundColour(colour=wx.WHITE)
 
-    def _initialise_sizer(self) -> None:
-        sizer = wx.BoxSizer(orient=wx.VERTICAL)
-        sizer.Add(window=self._panel, proportion=1, flag=wx.EXPAND)
-
-        self.SetSizer(sizer)
-        self.Layout()
-
     def set_panel(self, panel: wx.Panel) -> None:
         if self._panel:
-            self._replace(panel)
+            self.GetSizer().Replace(self._panel, panel)
+            self._panel = panel
 
         else:
-            self._create_panel(panel)
+            self._panel = panel
+
+            sizer = wx.BoxSizer(orient=wx.VERTICAL)
+            sizer.Add(window=self._panel, proportion=1, flag=wx.EXPAND)
+
+            self.SetSizer(sizer)
+            self.Layout()
 
         self.Layout()
-
-    def _create_panel(self, panel: wx.Panel) -> None:
-        self._panel = panel
-        self._initialise_sizer()
-
-    def _replace(self, panel: wx.Panel) -> None:
-        self.GetSizer().Replace(self._panel, panel)
-        self._panel = panel
