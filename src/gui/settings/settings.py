8eb3c74a2eb5aa81f1abc629ebf2ce25ea6be67a
RMvirtual <rmvirtual@AOL.com>
2023-12-22 21:25:59 +0000

    Inline settings constructor.

--- src/gui/settings/settings.py
+++ src/gui/settings/settings.py
@@ -9,17 +9,12 @@ class Settings(wx.Panel):
     def __init__(self, window: wx.Frame) -> None:
         super().__init__(parent=window)
 
-        self._initialise_widgets()
-        self._initialise_sizer()
-        self.SetBackgroundColour(colour=wx.WHITE)
+        self.title = wx.StaticText(parent=self, label="Settings")
+        self.title.SetFont(fonts.font(point_size=30, bold=True))
 
-    def _initialise_widgets(self) -> None:
-        self._initialise_title()
         self.directories = Directories(self)
         self.defaults = Defaults(self)
-        self._initialise_bottom_buttons()
 
-    def _initialise_bottom_buttons(self) -> None:
         self.save = wx.Button(parent=self, label="Save")
         self.exit = wx.Button(parent=self, label="Exit")
 
@@ -27,9 +22,9 @@ class Settings(wx.Panel):
         self.save.SetFont(font)
         self.exit.SetFont(font)
 
-    def _initialise_title(self) -> None:
-        self.title = wx.StaticText(parent=self, label="Settings")
-        self.title.SetFont(fonts.font(point_size=30, bold=True))
+        self._initialise_sizer()
+
+        self.SetBackgroundColour(colour=wx.WHITE)
 
     def _initialise_sizer(self) -> None:
         sizer = wx.BoxSizer(orient=wx.VERTICAL)
