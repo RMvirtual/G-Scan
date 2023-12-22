d42fd6288d9031d3b5aed39ad9bddcb8aa757377
RMvirtual <rmvirtual@AOL.com>
2023-12-22 21:37:34 +0000

    Inlined Settings Directory panel constructor.

--- src/gui/settings/settings.py
+++ src/gui/settings/settings.py
@@ -5,6 +5,75 @@ from gui.settings.directories import Directories
 from gui.settings.defaults import Defaults
 
 
+BrowseWidgets = tuple[wx.StaticText, wx.TextCtrl, wx.Button]
+
+
+class Directories(wx.Panel):
+    def __init__(self, parent: wx.Window) -> None:
+        super(Directories, self).__init__(parent=parent)
+
+        self.title = wx.StaticText(parent=self, label="Folders")
+        self.title.SetFont(fonts.font(point_size=20, bold=True))
+
+        self.scan_label, self.scan_box, self.scan_browse = \
+            self._browse_widgets("Scan Directory")
+
+        self.dest_label, self.dest_box, self.dest_browse = \
+            self._browse_widgets("Destination Directory")
+
+        font = fonts.font(point_size=12)
+
+        items = [
+            self.scan_label, self.scan_box, self.scan_browse,
+            self.dest_label, self.dest_box, self.dest_browse,
+        ]
+
+        for item in items:
+            item.SetFont(font)
+
+        sizer = wx.GridBagSizer(vgap=5, hgap=5)
+
+        alignment = wx.ALIGN_LEFT|wx.ALIGN_CENTRE_VERTICAL
+        align_and_expand = wx.EXPAND|alignment
+
+        sizer.Add(window=self.title, pos=(0, 0), span=(1,3), flag=alignment)
+
+        sizer.Add(window=self.scan_label, pos=(1, 0), flag=alignment)
+        sizer.Add(window=self.scan_box, pos=(1, 1), flag=align_and_expand)
+        sizer.Add(window=self.scan_browse, pos=(1, 2), flag=alignment)
+
+        sizer.Add(window=self.dest_label, pos=(2, 0), flag=alignment)
+        sizer.Add(window=self.dest_box, pos=(2, 1), flag=align_and_expand)
+        sizer.Add(window=self.dest_browse, pos=(2, 2), flag=alignment)
+
+        sizer.AddGrowableCol(1)
+        self.SetSizer(sizer)
+
+    @property
+    def scan_directory(self) -> str:
+        return self.scan_box.GetValue()
+
+    @scan_directory.setter
+    def scan_directory(self, directory: str) -> None:
+        self.scan_box.SetValue(directory)
+
+    @property
+    def dest_directory(self) -> str:
+        return self.dest_box.GetValue()
+
+    @dest_directory.setter
+    def dest_directory(self, directory: str) -> None:
+        self.dest_box.SetValue(directory)
+
+    def _browse_widgets(
+            self, label: str, box_value: str = "NULL") -> BrowseWidgets:
+        return (
+            wx.StaticText(parent=self, label=label),
+            wx.TextCtrl(parent=self, value=box_value),
+            wx.Button(parent=self, label="...")
+        )
+
+
 class Settings(wx.Panel):
     def __init__(self, window: wx.Frame) -> None:
         super().__init__(parent=window)
