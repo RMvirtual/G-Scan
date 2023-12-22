00e25f9b7ef19365984750069fb47e5354bacaed
RMvirtual <rmvirtual@AOL.com>
2023-12-22 22:37:46 +0000

    Logo panel inlining.

--- src/gui/logo_panel.py
+++ src/gui/logo_panel.py
@@ -1,44 +1,30 @@
 import wx
 import file_system
 
-from gui.metrics import aspect_ratio, scale_with_ratio
+from gui.metrics import scale_with_ratio
 
 
 class Logo(wx.Panel):
-    def __init__(self, parent: wx.Frame):
-        super(Logo, self).__init__(parent)
+    def __init__(self, parent: wx.Frame) -> None:
+        super().__init__(parent)
 
-        self._initialise_widgets()
-        self._initialise_sizer()
-        self.Bind(wx.EVT_SIZE, self.on_resize)
-
-    def _initialise_widgets(self) -> None:
-        image_dir = file_system.image_resources_directory()
-        image_path = image_dir.joinpath("logo.png")
-        self.image = wx.Image(str(image_path), wx.BITMAP_TYPE_PNG)
+        image = file_system.image_resources_directory().joinpath("logo.png")
+        self.image = wx.Image(str(image), wx.BITMAP_TYPE_PNG)
 
         self.bitmap = wx.StaticBitmap(
-            parent=self, bitmap = self.image.ConvertToBitmap(depth=32))
+            self, bitmap=self.image.ConvertToBitmap(depth=32))
 
-    def _initialise_sizer(self) -> None:
         sizer = wx.BoxSizer(orient=wx.VERTICAL)
-        flags = wx.EXPAND
-
-        sizer.Add(window=self.bitmap, proportion=0, flag=flags, border=0)
-
+        sizer.Add(window=self.bitmap, proportion=0, flag=wx.EXPAND, border=0)
         self.SetSizer(sizer)
 
-    def on_resize(self, _event = None) -> None:
-        self.resize_logo()
+        self.Bind(wx.EVT_SIZE, self.on_resize)
 
-    def resize_logo(self) -> None:
-        width, height = self._scaled_image_metrics()
+    def on_resize(self, _event = None) -> None:
+        width, height = self.Size
+        new_width, new_height = scale_with_ratio(self.image, width, height)
+
         scaled_image = self.image.Scale(
-            int(width), int(height), wx.IMAGE_QUALITY_NORMAL)
+            int(new_width), int(new_height), wx.IMAGE_QUALITY_NORMAL)
 
         self.bitmap.SetBitmap(scaled_image.ConvertToBitmap())
-
-    def _scaled_image_metrics(self) -> tuple[int, int]:
-        width, height = self.Size
-
-        return scale_with_ratio(self.image, width, height)
