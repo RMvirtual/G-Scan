6cb2948bec51aefe3abbf9d75c6d5038e3b1b25e
RMvirtual <rmvirtual@AOL.com>
2023-12-28 20:40:54 +0000

    Inline viewer bottom toolbar constructor.

--- src/gui/viewer/toolbars.py
+++ src/gui/viewer/toolbars.py
@@ -3,19 +3,13 @@ import wx
 
 class BottomToolbar(wx.Panel):
     def __init__(self, parent: wx.Frame):
-        super(BottomToolbar, self).__init__(
-            parent=parent
-        )
-        self.exit = wx.Button(parent=self, label="Exit")
-        self._initialise_sizer()
+        super(BottomToolbar, self).__init__(parent)
 
-    def _initialise_sizer(self) -> None:
+        self.exit = wx.Button(self, label="Exit")
         sizer = wx.BoxSizer(wx.VERTICAL)
 
         sizer.Add(
-            window=self.exit, proportion=0,
-            flag=wx.ALL|wx.ALIGN_RIGHT, border=5
-        )
+            self.exit, proportion=0, flag=wx.ALL|wx.ALIGN_RIGHT, border=5)
 
         self.SetSizer(sizer)
 
