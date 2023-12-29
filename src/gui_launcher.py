53e1707d14f6fe445d52b49110e443c85f55c832
RMvirtual <rmvirtual@AOL.com>
2023-12-29 18:43:35 +0000

    Inline JSON database factory function.

--- src/gui_launcher.py
+++ src/gui_launcher.py
@@ -39,23 +39,22 @@ def main() -> None:
     """Default entry point using current OS username logged in."""
 
     gui_runtime = wx.App()
+    config_directory = file_system.config_directory()   
     
-    app_config = AppConfiguration(_default_database(), os.getlogin())
-    controller = RootApplication(app_config)
+    database_files = list(map(
+        config_directory.joinpath,
+        ["departments.json", "document_types.json", "user_settings.json"]
+    ))
+
+    database = JSONDatabase(JSONDatabaseFiles(*database_files))
+    configuration = AppConfiguration(database, os.getlogin())
+
+    controller = RootApplication(configuration)
     controller.show()
     controller.launch_main_menu()
     
     gui_runtime.MainLoop()
 
-
-def _default_database() -> JSONDatabase:
-    database_files = [
-        "departments.json", "document_types.json", "user_settings.json"]
-
-    config_dir = file_system.config_directory()   
-    file_paths = list(map(config_dir.joinpath, database_files))
-
-    return JSONDatabase(JSONDatabaseFiles(*file_paths))
 
 
 if __name__ == '__main__':
