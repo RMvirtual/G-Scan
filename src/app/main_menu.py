df5cf97e1c0fdedf8719dff385ef094ea7bf399b
RMvirtual <rmvirtual@AOL.com>
2023-12-29 17:29:13 +0000

    More readable main menu contructor.

--- src/app/main_menu.py
+++ src/app/main_menu.py
@@ -10,51 +10,47 @@ class MainMenuController:
             self, root_application: RootInterface, app_config: AppConfiguration
     ) -> None:
         self._root = root_application
+        self._config = app_config
+
         self._gui = MainMenu(self._root.window)
         self._root.window.set_panel(self._gui)
 
-        departments_panel = self._gui.departments
+        self._gui.Bind(wx.EVT_CLOSE, self.on_close)
 
-        departments_panel.options.ops.Bind(wx.EVT_BUTTON, self.on_operations)
-        departments_panel.options.pods.Bind(
-            wx.EVT_BUTTON, self.on_credit_control)
-        
-        departments_panel.options.quick_start.Bind(
-            wx.EVT_BUTTON, self.on_quick_start)
-        
-        departments_panel.toolbar.settings.Bind(
-            wx.EVT_BUTTON, self.on_settings)
-        
-        departments_panel.toolbar.exit.Bind(wx.EVT_BUTTON, self.on_exit)
+        depts_panel = self._gui.departments
+
+        dept_options = self._gui.departments.options
+        dept_options.ops.Bind(wx.EVT_BUTTON, self.on_operations)
+        dept_options.pods.Bind(wx.EVT_BUTTON, self.on_credit_control)
+        dept_options.quick_start.Bind(wx.EVT_BUTTON, self.on_quick_start)
+
+        dept_toolbar =  depts_panel.toolbar
+        dept_toolbar.settings.Bind(wx.EVT_BUTTON, self.on_settings)
+        dept_toolbar.exit.Bind(wx.EVT_BUTTON, self.on_exit)
 
         ops_panel = self._gui.operations
-
-        ops_panel.options.cust_pwork.Bind(
-            wx.EVT_BUTTON, self.on_customer_paperwork)
-        
-        ops_panel.options.loading_list.Bind(
-            wx.EVT_BUTTON, self.on_loading_list)
-        
         ops_panel.back.Bind(wx.EVT_BUTTON, self.on_back_to_departments)
 
+        ops_options = ops_panel.options
+        ops_options.cust_pwork.Bind(wx.EVT_BUTTON, self.on_customer_paperwork)
+        ops_options.loading_list.Bind(wx.EVT_BUTTON, self.on_loading_list)
+        
         cc_panel = self._gui.credit_control
-
-        cc_panel.options.customer_paperwork_pod.Bind(
-            wx.EVT_BUTTON, self.on_signed_customer_paperwork)
-
-        cc_panel.options.signed_pod.Bind(wx.EVT_BUTTON, self.on_signed_pod)
         cc_panel.back.Bind(wx.EVT_BUTTON, self.on_back_to_departments)
 
-        self._gui.Bind(wx.EVT_CLOSE, self.on_close)
+        cc_options = cc_panel.options
+        cc_options.customer_paperwork_pod.Bind(
+            wx.EVT_BUTTON, self.on_signed_customer_paperwork)
+
+        cc_options.signed_pod.Bind(wx.EVT_BUTTON, self.on_signed_pod)
 
         f4_shortcut_id = wx.NewId()
         self._gui.Bind(wx.EVT_MENU, self.on_f4, id=f4_shortcut_id)
 
-        self._gui.SetAcceleratorTable(wx.AcceleratorTable([
-            (wx.ACCEL_NORMAL, wx.WXK_F4, f4_shortcut_id)]))
-        self._gui.SetFocus()
+        self._gui.SetAcceleratorTable(wx.AcceleratorTable([(
+            wx.ACCEL_NORMAL, wx.WXK_F4, f4_shortcut_id)]))
 
-        self._config = app_config
+        self._gui.SetFocus()
 
     def on_f4(self, event: wx.EVT_MENU) -> None:
         if self._gui.operations.IsShown():
