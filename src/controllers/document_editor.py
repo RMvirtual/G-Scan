c2649152fb59ce0b7f164d20d6be23255731691a
RMvirtual <rmvirtual@AOL.com>
2023-12-29 17:56:00 +0000

    Inline page view controller into source file.

--- src/controllers/document_editor.py
+++ src/controllers/document_editor.py
@@ -3,17 +3,18 @@ import ntpath
 import file_system
 import rendering
 
+from wx.lib.floatcanvas import FloatCanvas
 from controllers.abstract_root import RootInterface
 from controllers.configuration import AppConfiguration
-from controllers.page_view import PageViewController
 from controllers.user_input import SubmissionDocument, UserInputController
 from data_structures import AbstractNode, AbstractLeaf
 from document_tree import DocumentBranch, DocumentTree, JobBranch, PendingLeaf
 from document_type import DocumentType
 from job_references import JobReference
+from views import Viewer
 from views.page_range_dialog import PageRangeDialog
 from views.document_editor.document_tree import DocumentTreeCtrl
-from views import Viewer
+from views.document_editor.panels import PageView
 
 
 class DocumentTreeController:
@@ -156,6 +157,72 @@ class DocumentTreeController:
         )
 
 
+class PageViewController:
+    def __init__(self, gui: PageView):
+        self._gui = gui
+        self.canvas = gui.canvas
+
+        self._initialise_bindings()
+
+    def _initialise_bindings(self) -> None:
+        self.canvas.Bind(wx.EVT_MOUSEWHEEL, self.on_wheel)
+        self.canvas.Bind(wx.EVT_LEFT_DCLICK, self.fit_page_to_panel)
+
+    def clear_display(self) -> None:
+        self.canvas.ClearAll()
+        self.canvas.ZoomToBB()
+
+    def load_image(self, image: wx.Image) -> None:
+        bitmap = FloatCanvas.ScaledBitmap(
+            Bitmap=image, XY=(0,0), Height=image.GetHeight(), Position="bl")
+
+        self.clear_display()
+        self.canvas.AddObject(bitmap)
+        self.canvas.ZoomToBB()
+
+    def set_page_no(self, page_no) -> None:
+        self._gui.page_no.SetValue(page_no)
+
+    def set_total_pages(self, quantity: int or str) -> None:
+        self._gui.set_total_pages(quantity)
+
+    def on_wheel(self, event: wx.EVT_MOUSEWHEEL):
+        zoom_factor = (1 / 1.2) if event.GetWheelRotation() < 0 else 1.2
+
+        self.canvas.Zoom(
+            zoom_factor, event.Position, "Pixel", keepPointInPlace=True)
+
+    def fit_page_to_panel(self, _event: wx.EVT_LEFT_DCLICK = None):
+        self.canvas.ZoomToBB()
+
+    def bind_page_no(self, callback) -> None:
+        self._gui.page_no.Bind(event=wx.EVT_SPINCTRL, handler=callback)
+
+    def bind_delete(self, callback) -> None:
+        self._gui.delete_button.Bind(event=wx.EVT_BUTTON, handler=callback)
+
+    def bind_split_pages(self, callback) -> None:
+        self._gui.split_button.Bind(event=wx.EVT_BUTTON, handler=callback)
+
+    def show_all_widgets(self) -> None:
+        self._gui.delete_button.Show()
+        self._gui.split_button.Show()
+        self._gui.page_no.Show()
+        self._gui.page_quantity.Show()
+
+    def hide_all_widgets(self) -> None:
+        self._gui.delete_button.Hide()
+        self._gui.split_button.Hide()
+        self._gui.page_no.Hide()
+        self._gui.page_quantity.Hide()
+
+    def show_split_button(self) -> None:
+        self._gui.split_button.Show()
+
+    def hide_split_button(self) -> None:
+        self._gui.split_button.Hide()
+
+
 class DocumentController:
     def __init__(self, gui: Viewer):
         self._gui = gui
@@ -251,7 +318,6 @@ class DocumentEditorController:
     def __init__(
             self, root_application: RootInterface, config: AppConfiguration
     ) -> None:
-        
         self._root = root_application
         self._config = config
 
