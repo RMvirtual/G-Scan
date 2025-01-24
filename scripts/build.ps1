c375d1588a9f77321dcfa56825230af7ff5ed068
RMvirtual <rmatfen@gmail.com>
2025-01-24 18:42:28 +0000

    Add build script.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,18 @@
+[CmdletBinding()]
+param([switch] $clean)
+
 if (-Not $env:DEVENV) {& "$PSScriptRoot\setup.ps1"}
 
+$PYTHON_VENV = "$env:DEVENV\tools\python_venv.ps1"
 
-Write-Host "Should build here."
+Write-Host "Building release."
+
+Push-Location $env:DEVENV
+& $PYTHON_VENV -activate
+
+pyinstaller "$env:DEVENV\src\main.py" --name "gscan" --onedir --noconfirm `
+    --add-data "$env:DEVENV\data\*.json:data" `
+    $(if ($clean) {"--clean"})
+
+& $PYTHON_VENV -deactivate
+Pop-Location
