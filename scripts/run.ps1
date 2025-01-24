94ebd56da340b126d8b91618082ff99035d49701
RMvirtual <rmatfen@gmail.com>
2025-01-24 18:53:16 +0000

    Fill out run script for differentiating between src script development and the built exe file.

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,14 +1,38 @@
+[CmdletBinding()]
+param([switch] $src, [switch] $exe)
+
 if (-not $env:DEVENV) {& "$PSScriptRoot\setup.ps1"}
 
-$PYTHON_VENV = "$env:DEVENV\tools\python_venv.ps1"
 
+if ($src -and $exe) {
+    Write-Host "Must select only one option of -src or -exe."
+    exit 1
+}
 
-Write-Host "Running application from source code."
+if (-not $src -and -not $exe) {$src = $true}
 
-& $PYTHON_VENV -activate
-$env:GSCAN_ROOT = $env:DEVENV
+if ($src) {
+    Write-Host "Running application from source code."
 
-python.exe "$env:DEVENV\src\main.py"
+    $pythonVenv = "$env:DEVENV\tools\python_venv.ps1"
+    & $pythonVenv -activate
+    $env:GSCAN_ROOT = $env:DEVENV
 
-$env:GSCAN_ROOT = $NULL
-& $PYTHON_VENV -deactivate
+    python.exe "$env:DEVENV\src\main.py"
+
+    $env:GSCAN_ROOT = $NULL
+    & $pythonVenv -deactivate
+}
+
+elseif ($exe) {
+    $exeBinary = "$env:DEVENV\dist\gscan\gscan.exe"
+    
+    if (-not (Test-Path $exeBinary)) {
+        Write-Host "Cannot run exe file $exeBinary as it does not exist."
+
+        exit 1
+    }
+
+    & $exeBinary
+
+}
