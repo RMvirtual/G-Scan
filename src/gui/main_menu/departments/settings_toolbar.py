72d460a629a82dbb43c1bb6e1f260bec972a2446
RMvirtual <rmatfen@gmail.com>
2025-02-02 22:03:40 +0000

    Tidy up on settings toolbar.

--- src/gui/main_menu/departments/settings_toolbar.py
+++ src/gui/main_menu/departments/settings_toolbar.py
@@ -5,17 +5,19 @@ class SettingsToolbar(wx.Panel):
     def __init__(self, parent: wx.Frame):
         super().__init__(parent=parent)
 
-        self.settings = wx.Button(parent=self, label="Settings")
-        self.exit = wx.Button(parent=self, label="Exit")
+        font = wx.Font(wx.FontInfo(pointSize=30)).Bold()
+        
+        self.settings_btn = wx.Button(self, label="Settings")
+        self.settings_btn.SetFont(font)
+
+        self.exit_btn = wx.Button(self, label="Exit")
+        self.exit_btn.SetFont(font)
 
         sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        alignment_flags = wx.ALL|wx.ALIGN_RIGHT
 
-        for widget in [self.settings, self.exit]:
-            font = wx.Font(wx.FontInfo(pointSize=30)).Bold()
+        sizer.Add(
+            self.settings_btn, proportion=0, flag=alignment_flags, border=15)
 
-            widget.SetFont(font)
-
-            sizer.Add(
-                widget, proportion=0, flag=wx.ALL|wx.ALIGN_RIGHT, border=15)
-
+        sizer.Add(self.exit_btn, proportion=0, flag=alignment_flags, border=15)
         self.SetSizer(sizer)
