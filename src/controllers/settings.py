2f659ce005a3a6363acb79bd8ef20a266fb64fb3
RMvirtual <rmatfen@gmail.com>
2025-07-20 12:23:13 +0100

    More tidying of settings.

--- src/controllers/settings.py
+++ src/controllers/settings.py
@@ -3,21 +3,19 @@ import wx
 from configuration import Configuration
 from database import UserSettings
 from departments import Department
+from documents import DocumentType
 from gui.settings_dialog import SettingsDialog
 
 
 class SettingsDialogController:
-    def __init__(
-        self,
-        parent: wx.Frame,
-        config: Configuration,
-    ) -> None:
+    def __init__(self, parent: wx.Frame, config: Configuration) -> None:
         self.gui = SettingsDialog(parent)
         self.config = config
         self.user_settings = config.settings
 
         # Load settings from configuration.
         self.gui.output_directory_entry.SetValue(self.user_settings.output_dir)
+
         dept_box = self.gui.department_box
         dept_box.Set(list(map(lambda d: d.full_name, self.config.departments)))
         dept_box.SetValue(self.user_settings.department.full_name)
@@ -51,12 +49,14 @@ class SettingsDialogController:
         self.gui.SetAcceleratorTable(wx.AcceleratorTable(accelators))
 
     def update_options(self, department: Department) -> None:
-        names = [document.full_name for document in department.document_types]
+        self.gui.department_box.SetValue(department.full_name)
 
-        panel = self.gui
-        panel.department_box.SetValue(department.full_name)
-        panel.document_box.SetItems(names)
-        panel.document_box.SetValue(names[0])
+        document_types = [
+            document.full_name for document in department.document_types
+        ]
+
+        self.gui.document_box.Set(document_types)
+        self.gui.document_box.SetValue(document_types[0])
 
     def poll(self) -> None:
         self.gui.Show()
@@ -69,26 +69,11 @@ class SettingsDialogController:
         return None
 
     def on_save(self, event: wx.Event) -> None:
-        dept_value = self.gui.department_box.GetValue()
-
-        dept = list(
-            filter(
-                lambda d: d.full_name == dept_value, self.config.departments
-            )
-        )[0]
-
-        document_value = self.gui.document_box.GetValue()
-
-        document = list(
-            filter(
-                lambda d: d.full_name == document_value, dept.document_types
-            )
-        )[0]
-
-        output_directory = self.gui.output_directory_entry.GetValue()
-
         new_settings = UserSettings(
-            self.user_settings.username, output_directory, dept, document
+            self.user_settings.username,
+            self.gui.output_directory_entry.GetValue(),
+            self.department_from_box(),
+            self.document_type_from_box(),
         )
 
         self.user_settings.update(new_settings)
@@ -105,17 +90,36 @@ class SettingsDialogController:
             self.gui.output_directory_entry.SetValue(directory)
 
     def on_department_change(self, event: wx.Event) -> None:
-        current_name = self.gui.department_box.GetValue()
+        new_department = self.department_from_box()
 
-        matching_departments = filter(
-            lambda d: d.full_name == current_name, self.config.departments
-        )
-
-        department = list(matching_departments)[0]
-        self.update_options(department)
+        if new_department is not None:
+            self.update_options(new_department)
 
     def on_exit(self, event: wx.Event) -> None:
         self.gui.Destroy()
 
     def on_f4_escape_key(self, event: wx.Event) -> None:
         self.on_exit(event)
+
+    def department_from_box(self) -> Department | None:
+        current_name = self.gui.department_box.GetValue()
+
+        matching_departments = list(
+            filter(
+                lambda d: d.full_name == current_name, self.config.departments
+            )
+        )
+
+        return matching_departments[0] if matching_departments else None
+
+    def document_type_from_box(self) -> DocumentType | None:
+        current_name = self.gui.document_box.GetValue()
+
+        matching_types = list(
+            filter(
+                lambda d: d.full_name == current_name,
+                self.config.department.document_types,
+            )
+        )
+
+        return matching_types[0] if matching_types else None
