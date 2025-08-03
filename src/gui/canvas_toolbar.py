b9b6a26185cbb4788a40a47aa50447d36903da64
RMvirtual <rmatfen@gmail.com>
2025-08-03 11:58:08 +0100

    Add a generic zoom box.

--- src/gui/canvas_toolbar.py
+++ src/gui/canvas_toolbar.py
@@ -19,14 +19,26 @@ class CanvasToolbar(wx.Panel):
 
         self.camera_y_entry = wx.TextCtrl(self, value="0.0")
 
+        self.camera_zoom_lbl = wx.StaticText(self, label="Zoom")
+        zoom_options = [10, 25, 33, 50, 66, 75, 100, 125, 150, 200, 300, 500]
+
+        self.camera_zoom_combobox = wx.ComboBox(
+            self,
+            value="100%",
+            choices=[f"{level}%" for level in zoom_options],
+            style=wx.CB_READONLY,
+        )
+
         sizer = wx.BoxSizer()
-        label_pad = wx.ALIGN_CENTRE_VERTICAL | wx.RIGHT
+        label_style = wx.ALIGN_CENTRE_VERTICAL | wx.RIGHT
         border = 5
 
-        sizer.Add(self.camera_lbl, 0, label_pad, border)
-        sizer.Add(self.camera_x_lbl, 0, label_pad, border)
+        sizer.Add(self.camera_lbl, 0, label_style, border)
+        sizer.Add(self.camera_x_lbl, 0, label_style, border)
         sizer.Add(self.camera_x_entry, 0, wx.RIGHT, border)
-        sizer.Add(self.camera_y_lbl, 0, label_pad, border)
+        sizer.Add(self.camera_y_lbl, 0, label_style, border)
         sizer.Add(self.camera_y_entry, 0, wx.RIGHT, border)
+        sizer.Add(self.camera_zoom_lbl, 0, label_style, border)
+        sizer.Add(self.camera_zoom_combobox, 0, wx.RIGHT, border)
 
         self.SetSizerAndFit(sizer)
