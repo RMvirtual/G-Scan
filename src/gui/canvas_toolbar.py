50d995768b0da9b86ef0a3ce370af8d430d754bb
RMvirtual <rmatfen@gmail.com>
2025-08-03 11:39:06 +0100

    Add camera XY entries to the GUI.

--- src/gui/canvas_toolbar.py
+++ src/gui/canvas_toolbar.py
@@ -7,7 +7,26 @@ class CanvasToolbar(wx.Panel):
 
         self.camera_lbl = wx.StaticText(self, label="Camera")
 
+        self.camera_x_lbl = wx.StaticText(
+            self, label="X", style=wx.ALIGN_CENTER_HORIZONTAL
+        )
+
+        self.camera_x_entry = wx.TextCtrl(self, value="0.0")
+
+        self.camera_y_lbl = wx.StaticText(
+            self, label="Y", style=wx.ALIGN_CENTER_HORIZONTAL
+        )
+
+        self.camera_y_entry = wx.TextCtrl(self, value="0.0")
+
         sizer = wx.BoxSizer()
-        sizer.Add(self.camera_lbl)
+        label_pad = wx.ALIGN_CENTRE_VERTICAL | wx.RIGHT
+        border = 5
+
+        sizer.Add(self.camera_lbl, 0, label_pad, border)
+        sizer.Add(self.camera_x_lbl, 0, label_pad, border)
+        sizer.Add(self.camera_x_entry, 0, wx.RIGHT, border)
+        sizer.Add(self.camera_y_lbl, 0, label_pad, border)
+        sizer.Add(self.camera_y_entry, 0, wx.RIGHT, border)
 
         self.SetSizerAndFit(sizer)
